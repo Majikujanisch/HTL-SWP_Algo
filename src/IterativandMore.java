e8d5d978af7589b1c714d41e2e0f637fbc348a16
Majikujanisch <q1KGy6d8AaS0>
2020-10-12 19:10:28 +0200

    recursion Pow and Fab done

--- src/IterativandMore.java
+++ src/IterativandMore.java
@@ -3,6 +3,8 @@ public class IterativandMore {
 
         System.out.println(iterativSum(5));
         System.out.println(recursionSum(5));
+        System.out.println(recursionPow(2,3));
+        System.out.println(recursionFab(9));
     }
     public static int iterativSum(int anzahl){
         int sum = 0;
@@ -19,5 +21,17 @@ public class IterativandMore {
         else{
             return 0;
         }
+    }
+    public static double recursionPow(double zahl, int hoch){
+        if(hoch > 1){
+            return zahl * recursionPow(zahl, hoch-1);
+        }
+        return zahl;
+    }
+    public static int recursionFab(int anzahl){
+        if(anzahl > 1){
+            return recursionFab(anzahl -1) + recursionFab(anzahl -2);
+        }
+        return anzahl;
     }
 }
