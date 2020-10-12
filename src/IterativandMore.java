3e014079ea61d9a60cf3280426132db10f25a6bf
Majikujanisch <q1KGy6d8AaS0>
2020-10-12 11:47:37 +0200

    Done

--- src/IterativandMore.java
+++ src/IterativandMore.java
@@ -1,20 +1,20 @@
 public class IterativandMore {
     public static void main(String[] args) {
 
-        System.out.println(iterativSum(7));
-        System.out.println(recursionSum(7));
+        System.out.println(iterativSum(5));
+        System.out.println(recursionSum(5));
     }
     public static int iterativSum(int anzahl){
         int sum = 0;
-        for(int i = anzahl -1; i >= 1; i--){
+        for(int i = anzahl ; i >= 1; i--){
             sum = sum + i;
 
         }
         return sum;
     }
     public static int recursionSum(int anzahl){
-        if(anzahl > 1){
-            return anzahl-1 + recursionSum(anzahl-1);
+        if(anzahl > 0){
+            return anzahl + recursionSum(anzahl-1);
         }
         else{
             return 0;
